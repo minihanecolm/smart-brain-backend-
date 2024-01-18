46cb632d7fe940fdde02a42877e0c6611ed0ebad
minihanecolm <126052141+minihanecolm@users.noreply.github.com>
2024-01-18 15:28:32 +0000

    new json10

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,4 +1,4 @@
-const signIn =(db, bcrypt, eq, res)=>{
+const signIn =(db, bcrypt, req, res)=>{
 	const {email, password}= req.body;
 	if (!email || !password){
 		return res.status(400).json('incorrect submission')
